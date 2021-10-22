a02b384ddf352f4c0790b0aad0bebded0994a4a3
Nick Fisher <nickfish0311@gmail.com>
2021-10-22 10:12:44 +0100

    005: vending service layer interface

--- src/main/java/com/mthree/nick/vending/service/VendingServiceLayer.java
+++ src/main/java/com/mthree/nick/vending/service/VendingServiceLayer.java
@@ -1,4 +1,14 @@
 package com.mthree.nick.vending.service;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+
 public interface VendingServiceLayer {
+    HashMap<String, Item> getInventory();
+
+    void setInventory(HashMap<String, Item> inv);
+
+    boolean inStock(String name);
+
+    boolean sufficientMoney(BigDecimal amount, String name);
 }
